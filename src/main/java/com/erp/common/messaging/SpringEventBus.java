6ec04be37b6ebf9a777f40112b8b2c67b78fb4e5
junhyeong9812 <pickjog@gmail.com>
2026-04-20 21:22:53 +0900

    feat: 스프링이벤트 버스 추가

---
+++ src/main/java/com/erp/common/messaging/SpringEventBus.java
@@ -0,0 +1,20 @@
+package com.erp.common.messaging;
+
+import com.erp.common.domain.DomainEvent;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringEventBus implements EventBus {
+
+    private final ApplicationEventPublisher publisher;
+
+    public SpringEventBus(ApplicationEventPublisher publisher) {
+        this.publisher = publisher;
+    }
+
+    @Override
+    public void publish(DomainEvent event) {
+        publisher.publishEvent(event);
+    }
+}

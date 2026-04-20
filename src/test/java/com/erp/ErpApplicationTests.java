1bf7ab1013b9469691aa2b2b9994d1d66c2b9139
junhyeong9812 <pickjog@gmail.com>
2026-04-20 21:04:57 +0900

    test: ErpApplication 모듈간 경계 관련 테스트

--- src/test/java/com/erp/ErpApplicationTests.java
+++ src/test/java/com/erp/ErpApplicationTests.java
@@ -2,11 +2,26 @@ package com.erp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
 
 @SpringBootTest
 class ErpApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void verifyModules() {
+        ApplicationModules.of(ErpApplication.class).verify();
+    }
+
+    @Test
+    void writeDocumentation() {
+        ApplicationModules modules = ApplicationModules.of(ErpApplication.class);
+        new Documenter(modules)
+                .writeDocumentation()
+                .writeIndividualModulesAsPlantUml();
     }
 }

8680a9a547a6b7d24e7f25065e4bfd997375b080
junhyeong9812 <pickjog@gmail.com>
2026-05-22 08:21:13 +0900

    feat(hr): add EmployeeRepository findActive

--- src/main/java/com/erp/hr/application/port/outbound/EmployeeRepository.java
+++ src/main/java/com/erp/hr/application/port/outbound/EmployeeRepository.java
@@ -2,9 +2,11 @@ package com.erp.hr.application.port.outbound;
 
 import com.erp.hr.domain.entity.Employee;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface EmployeeRepository {
     Employee save(Employee employee);
     Optional<Employee> findById(Long id);
+    List<Employee> findActive();
 }
